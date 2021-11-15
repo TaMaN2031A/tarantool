f213b904f2ededa376cda54f0f709643b504efbf
vr009 <slavarianov@yandex.ru>
2021-11-15 11:58:28 +0300

    lua: fix SIGINT handling
    Tarantool console quits if you type Ctrl+C.
    This patch fixes console behavior on sending SIGINT.
    Console discards the current input on typing Ctrl+C
    and invites user to the new line.
    
    In daemon mode the process will exit after receiving SIGINT.
    
    Test gh-2717 should be skipped on release build, cause it
    uses error injection which is enabled only on debug mode.
    
    Fixes #2717
    
    @TarantoolBot document
    Title: Use Ctrl+C to discard the input in console
    
    The signal SIGINT discards the input in console mode.
    When tarantool executes with -e flag or runs as a daemon, SIGINT
    kills the process and tarantool complains about it in log.

--- test/unit/unit.c
+++ test/unit/unit.c
@@ -3,6 +3,20 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/*
+ * The definition of function set_sigint_cb is needed to avoid the error
+ * while linking. This occurs because libbox.a contains the unresolved
+ * lbox_console_readline() symbol, and it is necessary to construct a unit
+ * test executable.
+ */
+typedef struct ev_loop ev_loop;
+struct ev_signal;
+typedef void (*sigint_cb_t)(ev_loop *loop, struct ev_signal *w, int revents);
+sigint_cb_t
+set_sigint_cb(sigint_cb_t new_sigint_cb){
+	(void)new_sigint_cb;
+	return NULL;
+}
 
 enum { MAX_LEVELS = 10 };
 static int tests_done[MAX_LEVELS];
